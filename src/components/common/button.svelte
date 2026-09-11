43531d3de38e914b95bbe0637db0d69f7fc3375c
delphi <124740881+De1p0@users.noreply.github.com>
2026-09-10 22:16:34 -0400

    feat: expandable menu on click

--- src/components/common/button.svelte
+++ src/components/common/button.svelte
@@ -1,7 +1,25 @@
 <script>
 	export let Text;
+	export let label = "";
+	export let expand = false;
+	export let onclick = () => {};
 </script>
 
-<button class="hover:bg-primary-text/10 bg-surface p-1 rounded">
-	<Text strokeWidth={1.5} />
+<button
+	class="relative flex items-center gap-3 p-1 rounded
+        hover:bg-primary-text/10
+        {expand ? 'w-40' : 'w-fit'} overflow-hidden"
+	{onclick}
+>
+	<Text strokeWidth={1.3} />
+
+	{#if expand}
+		<span
+			class="
+		{expand ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-2'}
+	"
+		>
+			{label}
+		</span>
+	{/if}
 </button>
--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,43 +1,52 @@
 <script>
-	import { Bookmark, Clock, Menu, Search, Settings } from "@lucide/svelte";
+	import { Bookmark, Clock, Search, Settings } from "@lucide/svelte";
+
 	import Seperator from "./common/seperator.svelte";
 	import FillSpace from "./common/fill_space.svelte";
 	import Button from "./common/button.svelte";
+
+	let expand = $state(false);
 </script>
 
 <div
-	class="h-full bg-surface w-12 flex flex-col pt-2 items-center text-primary-text/90 gap-5"
+	class="
+        h-full bg-surface flex flex-col pt-2
+        text-primary-text/90 gap-5
+        overflow-hidden pl-2
+        transition-[width] duration-300 ease-in-out
+        {expand ? 'w-48' : 'w-12'}
+    "
 >
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke-width="1.5"
-		stroke="currentColor"
-		class="size-6"
-	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-		/>
-	</svg>
+	<div class="pl-1">
+		<svg
+			onclick={() => (expand = !expand)}
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6 cursor-pointer"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+			/>
+		</svg>
+	</div>
 
 	<Seperator />
 
-	<div class="gap-3 flex flex-col">
-		<!-- <Bookmark strokeWidth={1.5} />
-		<Clock strokeWidth={1.5} />
-		<Search strokeWidth={1.5} /> -->
-		<Button Text={Bookmark} />
-		<Button Text={Clock} />
-		<Button Text={Search} />
+	<div class="flex flex-col gap-3">
+		<Button Text={Bookmark} label="bookmarks" {expand} />
+		<Button Text={Clock} label="history" {expand} />
+		<Button Text={Search} label="search" {expand} />
 	</div>
 
 	<FillSpace />
+
 	<Seperator />
 
-	<div class="gap-5 flex flex-col mb-2">
-		<Button Text={Settings} />
+	<div class="flex flex-col gap-5 mb-2 px-3">
+		<Button Text={Settings} label="settings" {expand} />
 	</div>
 </div>
