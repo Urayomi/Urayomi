e1af071a0308e550b41ee66a1c9edf33264e4dfb
delphi <124740881+De1p0@users.noreply.github.com>
2026-09-17 21:26:26 -0400

    Test: Image loading test

--- src/lib/types/LibraryManga.ts
+++ src/lib/types/LibraryManga.ts
@@ -2,4 +2,5 @@ export interface LibraryManga {
 	location: string;
 	cover_location: string;
 	pages: string;
+	current_page: number;
 }
--- src/routes/read/+page.svelte
+++ src/routes/read/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+	import { invoke, convertFileSrc } from "@tauri-apps/api/core";
+
 	import { page } from "$app/state";
-	import { invoke } from "@tauri-apps/api/core";
+	import type { LibraryManga } from "$lib/types/LibraryManga";
 	const params = new URLSearchParams(page.url.search);
 
 	const book: string | null = params.get("book");
 
 	console.log(params.get("book"));
-	const data = await invoke("get_manga", { path: book });
+	const data: LibraryManga = await invoke("get_manga", { path: book });
 	console.log(data);
 </script>
 
-{book}
-<div></div>
+<div class="flex gap-3 w-full h-full">
+	<img
+		src={convertFileSrc(data.pages[0])}
+		class="aspect-2/3 flex-1 rounded object-cover"
+	/><img
+		src={convertFileSrc(data.pages[0])}
+		class="aspect-2/3 flex-1 rounded object-cover"
+	/>
+</div>
